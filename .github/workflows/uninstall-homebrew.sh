ade8d7b5ac361e4da5f6385445cb0e7f4bd05ae1
patricoferris <pf341@patricoferris.com>
2021-01-12 14:46:09 +0000

    fix linting errors

---
+++ .github/workflows/uninstall-homebrew.sh
@@ -0,0 +1,6 @@
+curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
+chmod +x ./uninstall.sh
+sudo ./uninstall.sh --force
+sudo rm -rf /usr/local/Homebrew
+sudo rm -rf /usr/local/Caskroom
+sudo rm -rf /usr/local/bin/brew
