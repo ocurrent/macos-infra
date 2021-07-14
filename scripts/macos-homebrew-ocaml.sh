4aa94a07d3159e22338441e74cac2200b491831e
Kate <kit.ty.kate@disroot.org>
2021-07-14 13:30:11 +0100

    Move the obuilder command setup from ~/.bash_profile to ~/.obuilder_profile.sh (sync change from patricoferris/obuilder#3)

--- scripts/macos-homebrew-ocaml.sh
+++ scripts/macos-homebrew-ocaml.sh
@@ -19,19 +19,25 @@ echo "Check opam"
 opam --version
 
 # Thanks Kate -- no opam init called just yet :)
-echo "Updating the bash_profile to pre-init OCaml"
-cat >> ./.bash_profile <<- EOM
-  case "'$USER'" in 
-  *-ocaml-4.12) export PATH="/Users/administrator/ocaml/4.12.0/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.11) export PATH="/Users/administrator/ocaml/4.11.1/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.10) export PATH="/Users/administrator/ocaml/4.10.2/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.09) export PATH="/Users/administrator/ocaml/4.09.1/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.08) export PATH="/Users/administrator/ocaml/4.08.1/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.07) export PATH="/Users/administrator/ocaml/4.07.1/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.06) export PATH="/Users/administrator/ocaml/4.06.1/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.05) export PATH="/Users/administrator/ocaml/4.05.0/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.04) export PATH="/Users/administrator/ocaml/4.04.2/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *-ocaml-4.03) export PATH="/Users/administrator/ocaml/4.03.0/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/local/bin";;
-  *) echo "Wrong user name. Can't find the ocaml version"; exit 1;;
-  esac
-EOM
+echo "Updating the .obuilder_profile.sh to pre-init OCaml"
+
+echo 'export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin' >> ./.obuilder_profile.sh
+
+case "$USER" in
+homebrew-*) echo 'export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH' >> ./.obuilder_profile.sh;; # /opt is used for homebrew on macOS/arm64
+*) echo "Distribution not supported"; exit 1;;
+esac
+
+case "\$USER" in
+homebrew-ocaml-4.12) echo 'export PATH=/Users/administrator/ocaml/4.12.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.11) echo 'export PATH=/Users/administrator/ocaml/4.11.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.10) echo 'export PATH=/Users/administrator/ocaml/4.10.2/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.09) echo 'export PATH=/Users/administrator/ocaml/4.09.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.08) echo 'export PATH=/Users/administrator/ocaml/4.08.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.07) echo 'export PATH=/Users/administrator/ocaml/4.07.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.06) echo 'export PATH=/Users/administrator/ocaml/4.06.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.05) echo 'export PATH=/Users/administrator/ocaml/4.05.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.04) echo 'export PATH=/Users/administrator/ocaml/4.04.2/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.03) echo 'export PATH=/Users/administrator/ocaml/4.03.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+*) echo "Can't find the ocaml version"; exit 1;;
+esac
