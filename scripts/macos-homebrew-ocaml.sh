aadc755f20828bba5acabb9501ecf1f5ecd73f55
Kate <kit.ty.kate@disroot.org>
2021-07-19 14:17:03 +0100

    fix the macos-homebrew-ocaml script: username wasn't correct

--- scripts/macos-homebrew-ocaml.sh
+++ scripts/macos-homebrew-ocaml.sh
@@ -27,22 +27,26 @@ echo "Updating the .obuilder_profile.sh to pre-init OCaml"
 
 echo 'export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin' >> ./.obuilder_profile.sh
 
-case "$USER" in
-macos-homebrew-*) echo 'export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH' >> ./.obuilder_profile.sh;; # /opt is used for homebrew on macOS/arm64
+# Username is expected to match tho following pattern: mac[0-9]+-.+
+# With the suffix part being what is passed by the user in (from suffix)
+USER_SUFFIX=$(echo "$USER" | cut -d- -f2-)
+
+case "$USER_SUFFIX" in
+homebrew-*) echo 'export PATH=/opt/homebrew/bin:/opt/homebrew/sbin:$PATH' >> ./.obuilder_profile.sh;; # /opt is used for homebrew on macOS/arm64
 *) echo "Distribution not supported"; exit 1;;
 esac
 
-case "\$USER" in
-macos-homebrew-ocaml-4.12) echo 'export PATH=/Users/administrator/ocaml/4.12.0/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.11) echo 'export PATH=/Users/administrator/ocaml/4.11.1/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.10) echo 'export PATH=/Users/administrator/ocaml/4.10.2/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.09) echo 'export PATH=/Users/administrator/ocaml/4.09.1/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.08) echo 'export PATH=/Users/administrator/ocaml/4.08.1/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.07) echo 'export PATH=/Users/administrator/ocaml/4.07.1/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.06) echo 'export PATH=/Users/administrator/ocaml/4.06.1/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.05) echo 'export PATH=/Users/administrator/ocaml/4.05.0/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.04) echo 'export PATH=/Users/administrator/ocaml/4.04.2/bin:$PATH' >> ./.obuilder_profile.sh;;
-macos-homebrew-ocaml-4.03) echo 'export PATH=/Users/administrator/ocaml/4.03.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+case "$USER_SUFFIX" in
+homebrew-ocaml-4.12) echo 'export PATH=/Users/administrator/ocaml/4.12.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.11) echo 'export PATH=/Users/administrator/ocaml/4.11.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.10) echo 'export PATH=/Users/administrator/ocaml/4.10.2/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.09) echo 'export PATH=/Users/administrator/ocaml/4.09.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.08) echo 'export PATH=/Users/administrator/ocaml/4.08.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.07) echo 'export PATH=/Users/administrator/ocaml/4.07.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.06) echo 'export PATH=/Users/administrator/ocaml/4.06.1/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.05) echo 'export PATH=/Users/administrator/ocaml/4.05.0/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.04) echo 'export PATH=/Users/administrator/ocaml/4.04.2/bin:$PATH' >> ./.obuilder_profile.sh;;
+homebrew-ocaml-4.03) echo 'export PATH=/Users/administrator/ocaml/4.03.0/bin:$PATH' >> ./.obuilder_profile.sh;;
 *) echo "Can't find the ocaml version"; exit 1;;
 esac
 
