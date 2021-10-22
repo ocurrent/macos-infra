89c070b9893ed11589ad0652b04fdec2287dc3c8
Patrick Ferris <pf341@patricoferris.com>
2021-10-22 15:30:22 +0100

    Delete .github/workflows directory
    Not using ansible so no need to test this!

--- .github/workflows/uninstall-homebrew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -sLO https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh
-chmod +x ./uninstall.sh
-sudo ./uninstall.sh --force
-sudo rm -rf /usr/local/Homebrew
-sudo rm -rf /usr/local/Caskroom
-sudo rm -rf /usr/local/bin/brew
