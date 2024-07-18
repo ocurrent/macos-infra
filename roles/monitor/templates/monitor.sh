d0edfdf551ad2d3ff6fe8c8d66073a2b0a449deb
Shon Feder <shon.feder@gmail.com>
2024-07-18 17:22:24 -0400

    Add process output to reboot message
    We think this may help us debug.
    
    Co-authored-by: Mark Elvers <mark.elvers@tunbury.org>
    Co-authored-by: Puneeth Chaganti <punchagan@muse-amuse.in>

--- roles/monitor/templates/monitor.sh
+++ roles/monitor/templates/monitor.sh
@@ -4,6 +4,7 @@ if test -f {{ ansible_env.HOME }}/ocluster.log ; then
 	kc=$(tail {{ ansible_env.HOME }}/ocluster.log | grep pkill | wc -l)
 	if [ $kc -eq 10 ] ; then
 		mv -f {{ ansible_env.HOME }}/ocluster.log {{ ansible_env.HOME }}/ocluster.old
+		msg="Rebooting $(uname -n)\n$(ps -j -U 1000)"
 		sync
 		sudo reboot
 	fi
